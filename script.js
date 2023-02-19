7219af41454abdf080ee5f8959507f92d6e794e0
Najaf Sharifov <nwerifov@mail.ru>
2023-02-19 21:42:13 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+//Task 2.6
+/*let id = prompt("enter product id:");
+switch (id) {
+  case "1":
+    alert("Available 10 pcs.");
+    break;
+  case "2":
+    alert("Available 256 pcs.");
+    break;
+  case "3":
+    alert("Available 53 pcs.");
+    break;
+  case "4":
+    alert("There are 3 available.");
+    break;
+  case "5":
+    alert("Out of stock");
+    break;
+};*/
+
+/*let name = prompt("Введите свой пол");
+let gender =  name === "M"? "male" : "female";
+console.log(gender);*/
+
+/*let month = prompt("Enter number of the month");
+
+switch (month) {
+  case "1":
+    console.log("January");
+    break;
+  case "2":
+    console.log("February");
+    break;
+  case "3":
+    console.log("March");
+    break;
+  case "4":
+    console.log("April");
+    break;
+  case "5":
+    console.log("May");
+    break;
+  case "6":
+    console.log("June");
+    break;
+  case "7":
+    console.log("July");
+    break;
+  case "8":
+    console.log("August");
+    break;
+  case "9":
+    console.log("September");
+    break;
+  case "10":
+    console.log("October");
+    break;
+  case "11":
+    console.log("November");
+    break;
+  case "12":
+    console.log("December");
+    break;
+  default:
+    console.log("Yanliş rəqəm daxil edilib");
+}*/
+
+//Task 2.6 Bonus
+/*let month = prompt("Enter month in the YYYY.MM.DD format");
+let monthFormat = month.split(".");
+
+switch (monthFormat[1]) {
+  case "01":
+    alert(monthFormat[2] + " january " + monthFormat[0] + "-cu il");
+    break;
+  case "02":
+    alert(monthFormat[2] + " february " + monthFormat[0] + "-cu il");
+    break;
+  case "03":
+    alert(monthFormat[2] + " march " + monthFormat[0] + "-cu il");
+    break;
+  case "04":
+    alert(monthFormat[2] + " april " + monthFormat[0] + "-cu il");
+    break;
+  case "05":
+    alert(monthFormat[2] + " may " + monthFormat[0] + "-cu il");
+    break;
+  case "06":
+    alert(monthFormat[2] + " june " + monthFormat[0] + "-cu il");
+    break;
+  case "07":
+    alert(monthFormat[2] + " july " + monthFormat[0] + "-cu il");
+    break;
+  case "08":
+    alert(monthFormat[2] + " august " + monthFormat[0] + "-cu il");
+    break;
+  case "09":
+    alert(monthFormat[2] + " september " + monthFormat[0] + "-cu il");
+    break;
+  case "10":
+    alert(monthFormat[2] + " october " + monthFormat[0] + "-cu il");
+    break;
+  case "11":
+    alert(monthFormat[2] + " november " + monthFormat[0] + "-cu il");
+    break;
+  case "12":
+    alert(monthFormat[2] + " december " + monthFormat[0] + "-cu il");
+    break;
+    default:
+        alert("Yanliş dəyər daxil edilib");
+}*/
